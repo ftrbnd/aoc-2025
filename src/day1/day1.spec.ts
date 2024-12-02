4eba286fba3874c6a8d4e3712f937b57e3364f55
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2024-12-01 21:01:11 -0800

    Complete day 1 part 2

--- src/day1/day1.spec.ts
+++ src/day1/day1.spec.ts
@@ -1,14 +1,18 @@
 import day1 from './index';
 
 describe('On Day 1', () => {
-	it(`Part 1: Get the total distance between the left and right lists`, () => {
-		const example = `3   4
+	const example = `3   4
 4   3
 2   5
 1   3
 3   9
 3   3`;
 
+	it(`Part 1: Get the total distance between the left and right lists`, () => {
 		expect(day1.solveForPartOne(example)).toBe(11);
+	});
+
+	it('Part 2: Get the similarity score', () => {
+		expect(day1.solveForPartTwo(example)).toBe(31);
 	});
 });
--- src/day1/index.ts
+++ src/day1/index.ts
@@ -5,7 +5,7 @@ class Day1 extends Day {
 		super(1);
 	}
 
-	solveForPartOne(input: string): number {
+	splitInput(input: string): number[][] {
 		const nums = input.split(/\s+/).map((s) => parseInt(s));
 
 		const left: number[] = [];
@@ -13,6 +13,12 @@ class Day1 extends Day {
 		nums.forEach((n, i) => (i % 2 === 0 ? left.push(n) : right.push(n)));
 		left.sort((a, b) => a - b);
 		right.sort((a, b) => a - b);
+
+		return [left, right];
+	}
+
+	solveForPartOne(input: string): string | number {
+		const [left, right] = this.splitInput(input);
 
 		let distance = 0;
 		for (let i = 0; i < Math.max(left.length, right.length); i++) {
@@ -23,8 +29,16 @@ class Day1 extends Day {
 		return distance;
 	}
 
-	solveForPartTwo(input: string): string {
-		return input;
+	solveForPartTwo(input: string): string | number {
+		const [left, right] = this.splitInput(input);
+
+		let score = 0;
+		for (let i = 0; i < left.length; i++) {
+			const count = right.filter((num) => num === left[i]);
+			score += left[i] * count.length;
+		}
+
+		return score;
 	}
 }
 
