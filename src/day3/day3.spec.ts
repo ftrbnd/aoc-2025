316e0ef363da4d5813b2a56db3bd924415ed2da6
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2024-12-02 22:16:24 -0800

    Complete day 3 part 2

--- src/day3/day3.spec.ts
+++ src/day3/day3.spec.ts
@@ -1,9 +1,13 @@
 import day3 from './index';
 
 describe('On Day 3', () => {
-	const example = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`;
-
 	it.skip(`Part 1: Add up all of the results of the real MUL instructions`, () => {
+		const example = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`;
 		expect(day3.solveForPartOne(example)).toBe(161);
+	});
+
+	it('Part 2: Add up all of the results of just the enabled multiplications', () => {
+		const example = `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`;
+		expect(day3.solveForPartTwo(example)).toBe(48);
 	});
 });
--- src/day3/index.ts
+++ src/day3/index.ts
@@ -5,29 +5,47 @@ class Day3 extends Day {
 		super(3);
 	}
 
+	solveMultiplication(instruction: string): number {
+		const [left, right] = instruction.split(',').map((str) => {
+			let num = '';
+			for (let i = 0; i < str.length; i++) {
+				if (!isNaN(parseInt(str[i]))) {
+					num += str[i];
+				}
+			}
+			return parseInt(num);
+		});
+
+		return left * right;
+	}
+
 	solveForPartOne(input: string): string | number {
 		const instructions = input.match(/(mul\(\d{1,3},\d{1,3}\))/g) ?? [];
+
 		let sum = 0;
-
 		for (const instruction of instructions) {
-			const [left, right] = instruction.split(',').map((str) => {
-				let num = '';
-				for (let i = 0; i < str.length; i++) {
-					if (!isNaN(parseInt(str[i]))) {
-						num += str[i];
-					}
-				}
-				return parseInt(num);
-			});
-
-			sum += left * right;
+			sum += this.solveMultiplication(instruction);
 		}
 
 		return sum;
 	}
 
 	solveForPartTwo(input: string): string | number {
-		return input;
+		const instructions =
+			input.match(/(mul\(\d{1,3},\d{1,3}\))|(don\'t\(\))|(do\(\))/g) ?? [];
+
+		let enabled = true;
+		let sum = 0;
+		for (const instruction of instructions) {
+			if (instruction === "don't()") enabled = false;
+			if (instruction === 'do()') enabled = true;
+
+			if (enabled && instruction !== "don't()" && instruction !== 'do()') {
+				sum += this.solveMultiplication(instruction);
+			}
+		}
+
+		return sum;
 	}
 }
 
