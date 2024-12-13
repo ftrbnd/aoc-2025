5d9d94992634d22f7908594ed51967dd86353c8d
ftrbnd <giosalas25@gmail.com>
2024-12-13 15:30:57 -0800

    Complete day 9 part 2

--- src/day9/day9.spec.ts
+++ src/day9/day9.spec.ts
@@ -1,8 +1,27 @@
 import day9 from './index';
 
 describe('On Day 9', () => {
-	it(`Part 1: Find the resulting filesystem checksum`, () => {
-		const example = `2333133121414131402`;
+	const example = `2333133121414131402`;
+
+	it.skip(`Part 1: Find the resulting filesystem checksum`, () => {
 		expect(day9.solveForPartOne(example)).toBe(1928);
 	});
+
+	it(`Part 2: Find the resulting filesystem checksum after moving whole files`, () => {
+		expect(day9.solveForPartTwo(example)).toBe(2858);
+	});
 });
+/**
+ * get the [start,end] of the next block
+ * check if any empty spaces can hold that block
+ */
+
+/**
+
+00...111...2...333.44.5555.6666.777.888899
+0099.111...2...333.44.5555.6666.777.8888..
+0099.1117772...333.44.5555.6666.....8888..
+0099.111777244.333....5555.6666.....8888..
+00992111777.44.333....5555.6666.....8888..
+
+ */
--- src/day9/index.ts
+++ src/day9/index.ts
@@ -45,11 +45,106 @@ class Day9 extends Day {
 		return blocks.filter((val) => val !== '.').map((val) => parseInt(val));
 	}
 
-	getCheckSum(nums: number[]): number {
+	getEmptySpaces(blocks: string[]): [number, number][] {
+		// [startIndex, endIndex]
+		const emptySpaces: [number, number][] = [];
+
+		for (let i = 0; i < blocks.length; i++) {
+			if (
+				blocks[i] === '.' &&
+				!emptySpaces.some(([start, end]) => start <= i && i <= end)
+			) {
+				let size = -1;
+				for (let j = i; blocks[j] === '.'; j++) {
+					size++;
+				}
+				emptySpaces.push([i, i + size]);
+			}
+		}
+
+		return emptySpaces;
+	}
+
+	getNextFile(blocks: string[], visited: number[]): [number, number] {
+		let fileEnd =
+			blocks.length -
+			[...blocks]
+				.reverse()
+				.findIndex((val) => val !== '.' && !visited.includes(parseInt(val))) -
+			1;
+
+		let fileStart = fileEnd;
+		for (let i = fileEnd; blocks[i] === blocks[fileEnd]; i--) {
+			fileStart = i;
+		}
+
+		return [fileStart, fileEnd];
+	}
+
+	moveFileToSpace(
+		blocks: string[],
+		file: [number, number],
+		space: [number, number]
+	) {
+		const fileSize = file[1] - file[0] + 1;
+		for (let i = space[0]; i < space[0] + fileSize; i++) {
+			blocks[i] = blocks[file[0]];
+		}
+
+		for (let i = file[0]; i < file[0] + fileSize; i++) {
+			blocks[i] = '.';
+		}
+	}
+
+	getFirstEmptySpace(blocks: string[], length: number): [number, number] {
+		for (let i = 0; i < blocks.length; i++) {
+			if (blocks[i] === '.') {
+				let size = 0;
+
+				for (let j = i; blocks[j] === '.'; j++) {
+					size++;
+				}
+
+				if (length <= size) {
+					return [i, size];
+				}
+			}
+		}
+
+		return [-1, 0];
+	}
+
+	fillSpacesByBlocks(blocks: string[]) {
+		const visited: number[] = [];
+		const uniqueFiles = new Set(
+			blocks.filter((v) => v !== '.').map((v) => parseInt(v))
+		);
+
+		while (visited.length < uniqueFiles.size) {
+			const [fileStart, fileEnd] = this.getNextFile(blocks, visited);
+			const fileSize = fileEnd - fileStart + 1;
+			visited.push(parseInt(blocks[fileEnd]));
+
+			const [spaceStart, spaceSize] = this.getFirstEmptySpace(
+				blocks,
+				fileEnd - fileStart + 1
+			);
+
+			if (fileSize <= spaceSize && spaceStart + spaceSize <= fileStart)
+				this.moveFileToSpace(
+					blocks,
+					[fileStart, fileEnd],
+					[spaceStart, spaceStart + spaceSize]
+				);
+		}
+	}
+
+	getCheckSum(nums: string[]): number {
 		let sum = 0;
 
 		for (let i = 0; i < nums.length; i++) {
-			sum += i * nums[i];
+			if (nums[i] === '.') continue;
+			sum += i * parseInt(nums[i]);
 		}
 
 		return sum;
@@ -58,13 +153,17 @@ class Day9 extends Day {
 	solveForPartOne(input: string): string | number {
 		const diskMap = input.split('').map((v) => parseInt(v));
 		const blocks = this.getFileBlocks(diskMap);
-		const swappedBlocks = this.fillSpaces(blocks);
+		this.fillSpaces(blocks);
 
-		return this.getCheckSum(swappedBlocks);
+		return this.getCheckSum(blocks);
 	}
 
 	solveForPartTwo(input: string): string | number {
-		return input;
+		const diskMap = input.split('').map((v) => parseInt(v));
+		const blocks = this.getFileBlocks(diskMap);
+		this.fillSpacesByBlocks(blocks);
+
+		return this.getCheckSum(blocks);
 	}
 }
 
